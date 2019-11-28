8a07ae27a8628f2aa0b4b381a733e241fe370519
Mxndarijn <40671102+Mxndarijn@users.noreply.github.com>
2019-11-28 20:43:05 +0100

    Add files via upload

---
+++ src/me/Mxndarijn/BleadNight/BlockPlaceListener.java
@@ -0,0 +1,30 @@
+package me.Mxndarijn.BleadNight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class BlockPlaceListener implements Listener {
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void BlockPlace(BlockPlaceEvent e) {
+		if(e.getPlayer().hasPermission("BleadNight.spBandiet")) {
+			if(e.getPlayer().getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase("§cBleadNight Spawnpoint Bandieten")) {
+				Location loc = e.getBlockPlaced().getLocation();
+				List<String> Spawnpoints = BleadNight.Main.getConfig().getStringList("SpawnPoints.Bandieten.SpawnPoints");
+				if(Spawnpoints == null) Spawnpoints = new ArrayList<String>();
+				Spawnpoints.add(loc.getX() + "-" + loc.getY() + "-" + loc.getZ() + "-" + loc.getWorld().getName());
+				BleadNight.Main.getConfig().set("SpawnPoints.Bandieten.SpawnPoints", Spawnpoints);
+				BleadNight.Main.saveConfig();
+				e.getPlayer().sendMessage("§cBleadNight §7Spawnpoint toegevoegd (§c" + loc.getX() + ", " + loc.getY() + ", " + loc.getZ() + ", " + loc.getWorld().getName() + "§7)");
+				
+			}
+		}
+	}
+
+}
--- src/me/Mxndarijn/BleadNight/InventoryListener.java
+++ src/me/Mxndarijn/BleadNight/InventoryListener.java
@@ -18,6 +18,8 @@ import org.bukkit.inventory.meta.ItemMeta;
 @SuppressWarnings("deprecation")
 public class InventoryListener implements Listener {
 	public HashMap<Player, Boolean> ChangeHealth = new HashMap<Player, Boolean>();
+	public HashMap<Player, Boolean> ChangeDamage = new HashMap<Player, Boolean>();
+	public HashMap<Player, Boolean> ChangeAantal = new HashMap<Player, Boolean>();
 	
 	//BleadNight Main Menu
 	@EventHandler
@@ -41,7 +43,7 @@ public class InventoryListener implements Listener {
 	    	    lore.add("§cHealth§8: §7" + BleadNight.Main.getConfig().getInt("Bandieten.Health"));
 	    	    im.setLore(lore);
 				is.setItemMeta(im);
-				inv.setItem(10, is);
+				inv.setItem(9, is);
 				
 				is = new ItemStack(Material.BEDROCK, 1, (short) 0);
 				im = is.getItemMeta();
@@ -49,15 +51,18 @@ public class InventoryListener implements Listener {
 				im.addItemFlags(ItemFlag.HIDE_ENCHANTS);
 				im.setDisplayName("§cSpawnPoints");
 				is.setItemMeta(im);
-				inv.setItem(12, is);
+				inv.setItem(11, is);
 				
 				is = new ItemStack(Material.IRON_SWORD, 1, (short) 0);
 				im = is.getItemMeta();
 				im.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
 				im.addItemFlags(ItemFlag.HIDE_ENCHANTS);
 				im.setDisplayName("§7Damage");
+				lore.clear();
+				lore.add("§cDamage§8: §7" + BleadNight.Main.getConfig().getInt("Bandieten.Damage"));
+	    	    im.setLore(lore);
 				is.setItemMeta(im);
-				inv.setItem(14, is);
+				inv.setItem(13, is);
 				
 				is = new ItemStack(Material.CHEST, 1, (short) 0);
 				im = is.getItemMeta();
@@ -65,7 +70,18 @@ public class InventoryListener implements Listener {
 				im.addItemFlags(ItemFlag.HIDE_ENCHANTS);
 				im.setDisplayName("§7Loot");
 				is.setItemMeta(im);
-				inv.setItem(16, is);
+				inv.setItem(15, is);
+				
+				is = new ItemStack(Material.MONSTER_EGG, 1, (short) 2);
+				im = is.getItemMeta();
+				im.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+				im.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+				im.setDisplayName("§7Aantal er Spawnen");
+				lore.clear();
+				lore.add("§cAantal Er Spawnen§8: §7" + BleadNight.Main.getConfig().getInt("Bandieten.AantalErSpawnen"));
+	    	    im.setLore(lore);
+				is.setItemMeta(im);
+				inv.setItem(17, is);
 				
 				p.openInventory(inv);
 				
@@ -82,11 +98,31 @@ public class InventoryListener implements Listener {
 			if(e.getInventory().getName().contains("§7BleadNight Settings§f")) {
 				Player p = (Player) e.getWhoClicked();
 				e.setCancelled(true);
-				if(e.getSlot() == 10) {
+				if(e.getSlot() == 9) {
 					p.closeInventory();
 					p.sendMessage("§cBleadNight §7Typ de health die de bandieten krijgen. (Hele getallen bv. 221) of typ§c cancel");
 					ChangeHealth.put(p, true);
-					
+				}
+				if(e.getSlot() == 11) {
+					p.sendMessage("§cBleadNight §7Plaats dit block om een spawnpoint te zetten.");
+					ItemStack is = new ItemStack(Material.BEDROCK, 1, (short) 0);
+					ItemMeta im = is.getItemMeta();
+					im.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+					im.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+					im.setDisplayName("§cBleadNight Spawnpoint Bandieten");
+					is.setItemMeta(im);
+					e.getWhoClicked().getInventory().addItem(is);
+					p.closeInventory();
+				}
+				if(e.getSlot() == 13) {
+					p.closeInventory();
+					p.sendMessage("§cBleadNight §7Typ de damage die de bandieten doen. (Hele getallen bv. 221) of typ§c cancel");
+					ChangeDamage.put(p, true);
+				}
+				if(e.getSlot() == 17) {
+					p.closeInventory();
+					p.sendMessage("§cBleadNight §7Typ hoeveel bandieten er spawnen. (Hele getallen bv. 221) of typ§c cancel");
+					ChangeAantal.put(p, true);
 				}
 			}
 		}
@@ -96,7 +132,7 @@ public class InventoryListener implements Listener {
 				e.setCancelled(true);
 				Player p = e.getPlayer();
 				ChangeHealth.remove(p);
-				int i = 30;
+				int i = 1;
 				if(e.getMessage().equalsIgnoreCase("cancel")) {
 					p.sendMessage("§cBleadNight §7canceled");
 					return;
@@ -107,11 +143,57 @@ public class InventoryListener implements Listener {
 				}catch(NumberFormatException ee) {
 					p.sendMessage("§cBleadNight §7Typ de health die de bandieten krijgen. (Hele getallen bv. 221)");
 					ChangeHealth.put(p, true);
+					return;
 				}
-				System.out.println(i);
 				BleadNight.Main.getConfig().set("Bandieten.Health", i);
 				BleadNight.Main.saveConfig();
 				p.sendMessage("§cBleadNight §7Health is verzet naar §c" + i + "❤");
+				
+				
+			}
+			if(ChangeDamage.containsKey(e.getPlayer())) {
+				e.setCancelled(true);
+				Player p = e.getPlayer();
+				ChangeDamage.remove(p);
+				int i = 1;
+				if(e.getMessage().equalsIgnoreCase("cancel")) {
+					p.sendMessage("§cBleadNight §7canceled");
+					return;
+				}
+				try {
+					String mes = e.getMessage();
+					i = (int) Integer.parseInt(mes);
+				}catch(NumberFormatException ee) {
+					p.sendMessage("§cBleadNight §7Typ de health die de bandieten krijgen. (Hele getallen bv. 221)");
+					ChangeDamage.put(p, true);
+					return;
+				}
+				BleadNight.Main.getConfig().set("Bandieten.Damage", i);
+				BleadNight.Main.saveConfig();
+				p.sendMessage("§cBleadNight §7Damage is verzet naar §c" + i);
+				
+				
+			}
+			if(ChangeAantal.containsKey(e.getPlayer())) {
+				e.setCancelled(true);
+				Player p = e.getPlayer();
+				ChangeAantal.remove(p);
+				int i = 1;
+				if(e.getMessage().equalsIgnoreCase("cancel")) {
+					p.sendMessage("§cBleadNight §7canceled");
+					return;
+				}
+				try {
+					String mes = e.getMessage();
+					i = (int) Integer.parseInt(mes);
+				}catch(NumberFormatException ee) {
+					p.sendMessage("§cBleadNight §7Typ hoeveel bandieten er spawnen. (Hele getallen bv. 221)");
+					ChangeAantal.put(p, true);
+					return;
+				}
+				BleadNight.Main.getConfig().set("Bandieten.AantalErSpawnen", i);
+				BleadNight.Main.saveConfig();
+				p.sendMessage("§cBleadNight §7Aantal is verzet naar §c" + i);
 				
 				
 			}
